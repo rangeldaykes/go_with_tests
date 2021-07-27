5bb0dead715352ab7c44a54a3f2e3998cc028c16
rangeldaykes <daykes@gmail.com>
2021-07-27 11:16:37 -0300

    array_slices

--- gwt_03_iteration/repeat_test.go
+++ gwt_03_iteration/repeat_test.go
@@ -1,11 +1,8 @@
 package gwt_03_iteration
 
-import (
-	"fmt"
-	"testing"
-)
+import "testing"
 
-func TestRepeat( t *testing.T) {
+func TestRepeat(t *testing.T) {
 	repeated := Repeat("a", 5)
 	expected := "aaaaa"
 
@@ -14,14 +11,8 @@ func TestRepeat( t *testing.T) {
 	}
 }
 
-func BenchmarkRepeat (b *testing.B) {
+func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Repeat("a", 5)
 	}
-}
-
-func ExampleRepeat() {
-	repeated := Repeat("a", 5)
-	fmt.Println(repeated)
-	//Output: aaaaa
 }
